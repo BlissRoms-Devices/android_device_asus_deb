bf7fb7911c49f97025438ff6c154d49119fc8e15
Aren Clegg <aclegg2011@gmail.com>
2016-09-10 20:59:29 -0700

    blissify

---
+++ bliss.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/bliss/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/aosp_deb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := bliss_deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razorg \
+     BUILD_FINGERPRINT=google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys \
+     PRIVATE_BUILD_DESC="razorg-user 6.0.1 MOB30X 3036618 release-keys"
+
